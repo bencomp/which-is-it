d93e0d9072f98d1e8bdec21fffafc612179cc18c
Mark Lindeman <mark@lindeman.nu>
2022-11-05 01:53:27 +0100

    Implements a basic UI for our game.

---
+++ app/game.js
@@ -0,0 +1,124 @@
+const api = 'https://api.data.netwerkdigitaalerfgoed.nl/datasets/NMVW/collectie/services/collectie/sparql'
+let player
+const postOptions = {
+  method: 'POST',
+  mode: 'cors',
+  cache: 'no-cache',
+  redirect: 'follow',
+  headers: {
+    "Accept": "application/sparql-results+json",
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+}
+window.onload = function() {
+  fetch('getobjects.rq')
+    .then(response => response.text())
+    .then(rq => {
+      postOptions.body = `query=${rq}`
+      fetch(api, postOptions)
+        .then(response => response.json())
+        .then(ld => {
+          const html = []
+          ld.results.bindings.forEach(obj => {
+            const img = new Image()
+            img.onload = () => {
+              if (this.width > this.height) {
+                
+              }
+            }
+            img.src = obj.img.value
+            html.push(`
+    <article class="col" id="${obj.id.value}">
+      <div class="card shadow-sm">
+        <img src="${obj.img.value}">
+        <div class="card-body">
+          <p class="card-text">${obj.onderwerp.value}, <a href="${obj.geonames.value}">${obj.lokatie.value}</a></p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="guess btn btn-sm btn-outline-secondary">Deze is het!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+`)
+          })
+          document.getElementById('objects').innerHTML = html.join('')
+          document.querySelectorAll('article').forEach(obj => {
+            obj.onclick = (ev) => {
+              obj.classList.toggle('selected')
+            }
+          })
+
+          document.querySelectorAll('.guess').forEach(obj => {
+            obj.onclick = (e) => {
+               e.preventDefault()
+               return false
+            }
+          })
+        })
+        .catch(e => alert('Er heeft zich een fout voorgedaan'))
+    })
+
+  const eventOut = {
+        type: "question",
+        question: "Hello world",
+        player: player,
+    }
+    // setTimeout(() => {
+    //   console.lwebsocket.send(JSON.stringify(eventOut))
+    //   alert(1)
+    // }, 1000)
+    // 
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const websocket = new WebSocket("ws://172.16.32.159:8001/");
+  initGame(websocket)
+})
+
+function initGame(websocket) {
+  websocket.addEventListener("open", () => {
+    // Send an "init" event according to who is connecting.
+    const params = new URLSearchParams(window.location.search);
+    let event = { type: "init" };
+    if (params.has("join")) {
+      // Second player joins an existing game.
+      event.join = "player2";
+      player = "player2";
+    } else if (params.has("watch")) {
+      // Spectator watches an existing game.
+      event.watch = "watch";
+    } else {
+      // First player starts a new game.
+      player = "player1";
+    }
+    // FIXME: show player1 or player2 in UI
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+
+
+/**
+ * Update UI with "yes"/"no" answer
+ * @param {string} answer The other player's answer
+ */
+function showAnswer(answer) {
+    // FIXME
+}
+
+/**
+ * Update UI with question to answer
+ * @param {string} question The other player's question
+ */
+function showQuestion(question) {
+    // FIXME
+}
+/**
+ * Update UI with number of images the opponent has left
+ * @param {number} numImages Number of images opponent has left
+ */
+function showOpponentImages(numImages) {
+    // FIXME
+}
